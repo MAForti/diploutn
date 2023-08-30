3595f856eb323c5aeea71718b841dfd25b2170a2
Mauro Forti <data@live.com.ar>
2023-08-30 16:45:51 -0300

    agregados los cambios de la clase 17 a la clase 19

--- clase19/app.js
+++ clase19/app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const path = require('path');
 const app = express()
 const mysql = require('mysql');
+const bodyParser = require('body-parser');
 const port = 3000
 
 // Creamos la conexion con MySQL
@@ -25,6 +26,31 @@ con.connect(function(err) {
   });
 
 });
+
+// Habilita capturar datos por POST
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Devuelve el login.html
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/login.html'));
+})
+
+// Recibiendo por POST
+app.post('/login', (req, res) => {
+  var email = req.body.email;
+  var password = req.body.password;
+  console.log(email);
+  console.log(password);
+
+  if (email == "a@a.com" && password == "1") {
+    // Email y contraseña validas
+    res.sendFile(path.join(__dirname, '/panel.html'));
+  } else {
+    // Email y contraseña incorrecta
+    res.sendFile(path.join(__dirname, '/login.html'));
+  }
+  
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
